85c0fdd5d79bb0ea33dac410bb286f230a27479a
KingRavo18 <rkingovskis14@gmail.com>
2024-11-25 14:03:27 +0200

    r

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
-
+    extend: {
+      keyframes: {
+        fadeSlideUp: {
+          '0%': { opacity: '0', transform: 'translateY(100px)' }, // Start further down
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeSlideUp: 'fadeSlideUp 0.1s ease-out forwards',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
